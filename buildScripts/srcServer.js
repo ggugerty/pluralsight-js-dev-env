8eb32e71ab3734aefcfaadd6d0aa41fb4a5bc078
Gregg Gugerty <gregg.gugerty@gmail.com>
2018-09-28 13:28:55 -0500

    Set up automation using npm scripts

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var open = require('open');
+var opn = require('opn');
 
 var port = 3000;
 var app = express();
@@ -13,6 +13,6 @@ app.listen(port, function(err) {
     if (err) {
         console.log(err);
     } else {
-        open('http://localhost:' + port);
+        opn('http://localhost:' + port);
     }
 });
